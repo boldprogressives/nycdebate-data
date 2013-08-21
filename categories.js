93d2a6b908a730d4bc3f1cd03381c7d82dc08c88
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-08-21 13:46:02 -0400

    updating data

---
+++ categories.js
@@ -0,0 +1,18 @@
+populateCategories([
+  {
+    "num": 1, 
+    "name": "_other"
+  }, 
+  {
+    "num": 1, 
+    "name": "testing"
+  }, 
+  {
+    "num": 3, 
+    "name": "robots"
+  }, 
+  {
+    "num": 8, 
+    "name": "meta"
+  }
+]);
